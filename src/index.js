f54f1b785d0c1cbfcc202064f31e3b3cbc9150d7
hjoshi13 <honey.joshi@inferenz.ai>
2024-08-16 23:36:29 +0530

    [UPD]hash routing

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./i18n";
 import { HeaderProvider } from "./Context/Provider/HeaderProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HeaderProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </HeaderProvider>
 );
